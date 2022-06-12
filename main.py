3fecfe2bc4f7ebe581b193668f7d6e3c8d9489a9
Przybs99 <102868064+Przybs99@users.noreply.github.com>
2022-06-12 09:29:06 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -1,3 +1,24 @@
+import csv, sqlite3
 
 if __name__ == "__main__":
-    pass
+    con = sqlite3.connect(":memory:", detect_types=sqlite3.PARSE_DECLTYPES|sqlite3.PARSE_COLNAMES)
+    cursor = con.cursor()
+    cursor.execute('''CREATE TABLE polaczenia (from_subscriber data_type INTEGER, 
+                    to_subscriber data_type INTEGER, 
+                    datetime data_type timestamp, 
+                    duration data_type INTEGER , 
+                    celltower data_type INTEGER);''')
+
+    with open ("polaczenia.csv", 'r') as a:
+        file = csv.reader(a, delimiter=';')
+        next(file, None)
+        num_rows = [var for var in file]
+        cursor.executemany("INSERT INTO polaczenia (from_subscriber, to_subscriber, datetime, duration , celltower) VALUES (?, ?, ?, ?, ?);", num_rows)
+        
+
+con
+c = con.cursor()
+c.execute("SELECT SUM(DURATION) FROM POLACZENIA")
+result = c.fetchone()[0]
+
+print(int(result))
